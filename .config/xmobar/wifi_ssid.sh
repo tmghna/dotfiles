2cf257120d2e895052aada1040c8cda1d1add095
tmghna <tamaghnadey1916@gmail.com>
2026-03-18 21:51:46 +0530

    feat: progress on xmobar aesthetics, Qt mauve dark theme

---
+++ .config/xmobar/wifi_ssid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fetch the active SSID
+ssid=$(nmcli -t -f active,ssid dev wifi | awk -F: '/^yes/ {print $2}')
+
+# Check if the string is empty
+if [ -z "$ssid" ]; then
+    # Disconnected: Red No-WiFi Icon, no SSID
+    echo "<fc=#f38ba8>󰖪</fc> "
+else
+    # Connected: Green WiFi Icon + SSID
+    echo "<fc=#a6e3a1>󰖩</fc> $ssid"
+fi
