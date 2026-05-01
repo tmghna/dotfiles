68915928e6f03c91159166a32bf6da8583158c01
tmghna <tamaghnadey1916@gmail.com>
2026-05-01 11:38:55 +0530

    notification timings tweak, caffeine toggle notif addition

--- .config/xmonad/scripts/caffeine_toggle.sh
+++ .config/xmonad/scripts/caffeine_toggle.sh
@@ -2,15 +2,17 @@
 STATE_FILE="/tmp/caffeine_mode"
 
 if [ -f "$STATE_FILE" ]; then
-    # It is ON. Turn it OFF (Allow sleep & screen blanking)
-    rm "$STATE_FILE"
-    xautolock -enable
-    xset s on         # Enable screensaver
-    xset +dpms        # Enable Display Power Management
+  # It is ON. Turn it OFF (Allow sleep & screen blanking)
+  rm "$STATE_FILE"
+  xautolock -enable
+  xset s on  # Enable screensaver
+  xset +dpms # Enable Display Power Management
+  notify-send -u normal "☕ Caffeine" "Mode Deactivated. Normal sleep resumed."
 else
-    # It is OFF. Turn it ON (Prevent sleep & screen blanking)
-    touch "$STATE_FILE"
-    xautolock -disable
-    xset s off        # Disable screensaver
-    xset -dpms        # Disable Display Power Management
+  # It is OFF. Turn it ON (Prevent sleep & screen blanking)
+  touch "$STATE_FILE"
+  xautolock -disable
+  xset s off # Disable screensaver
+  xset -dpms # Disable Display Power Management
+  notify-send -u normal "☕ Caffeine" "Mode Activated. Screen will stay awake."
 fi
